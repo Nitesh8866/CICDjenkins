8ac44c4bb004fd85b545a5c32e9af50ee567206b
Nitesh8866 <89569412+Nitesh8866@users.noreply.github.com>
2025-12-07 10:29:24 +0530

    Update S3 bucket and region in backend configuration

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "mytodobucketapp88665"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 }
